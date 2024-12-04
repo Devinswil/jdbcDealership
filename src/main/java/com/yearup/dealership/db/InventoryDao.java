0959e3d2a17ff3c9a01e8fd27454d4e7cc2da5ba
DevinW <devinswilson03@gmail.com>
2024-12-04 13:40:30 -0500

    finished add vehicle to inventory method

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -3,6 +3,7 @@ package com.yearup.dealership.db;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class InventoryDao {
@@ -14,6 +15,14 @@ public class InventoryDao {
 
     public void addVehicleToInventory(String vin, int dealershipId) {
         // TODO: Implement the logic to add a vehicle to the inventory
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement=connection.prepareStatement("INSERT INTO inventory (VIN,DealershipID) VALUES (?,?) ")) {
+            statement.setString(1,vin);
+            statement.setInt(2,dealershipId);
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
