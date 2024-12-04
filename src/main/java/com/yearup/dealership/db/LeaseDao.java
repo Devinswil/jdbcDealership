f97ce030118808d502f34ea8875aed6c29c9745c
DevinW <devinswilson03@gmail.com>
2024-12-04 14:19:23 -0500

    finiahed adding lease method

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -4,6 +4,7 @@ import com.yearup.dealership.models.LeaseContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -16,5 +17,18 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+        String query= "INSERT INTO lease_contracts ( VIN, lease_start, lease_end, monthly_payment) VALUES (?, ?, ?, ?, ?)";
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement=connection.prepareStatement(query)) {
+            statement.setString(1, leaseContract.getVin());
+            statement.setDate(2, Date.valueOf(leaseContract.getLeaseStart()));
+            statement.setDate(3, Date.valueOf(leaseContract.getLeaseEnd()));
+            statement.setDouble(4,leaseContract.getMonthlyPayment());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+    }
+
